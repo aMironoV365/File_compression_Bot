a019e1659d81eb06850697227b702ed233a71be2
Andrey <A.mironov95@mail.ru>
2024-09-28 18:39:16 +0300

    logger updated

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from handlers.custom_handlers.handler_compress_pdf import compress_pdf
 from handlers.default_handlers.commands import send_commands
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filemode='w',
+                    filename='compression_bot.log',
+                    datefmt='%d/%m/%Y %H:%M:%S')
 
 
 async def main():
